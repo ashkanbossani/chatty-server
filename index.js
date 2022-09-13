5661c669e13a2b91d62052485f98d947fa030515
ashkanbossani <abossani@hotmail.com>
2022-09-13 14:54:18 -0700

    fixed connection to mongoDB by adding connection string

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const connectionstring = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sdk1o7l.mongodb.net/test`;
+
+
+mongoose.connect(connectionstring, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err.message);
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
