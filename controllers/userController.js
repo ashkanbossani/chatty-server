1ed13388c17cccb63f828820707d5cb105667404
ashkanbossani <abossani@hotmail.com>
2022-09-14 16:30:13 -0700

    added bcrypt library for encryption of password and added validation on server side for user name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,25 @@
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
 module.exports.register = (req, res, next) => {
-    const {username, email, password} = req.body;
-}
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = User.findOne({ username });
+    if (usernameCheck)
+      return res.status(400).json({ message: "Username already exists" });
+    const emailCheck = User.findOne({ email });
+    if (emailCheck)
+      return res.status(400).json({ message: "Email already exists" });
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    delete newUser.password;
+    return res.json({ status: true, newUser})
+  } catch (ex) {
+    next(ex);
+  }
+};
