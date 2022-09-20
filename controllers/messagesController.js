d5f64404aef422703160cae737ffad2d00dc19ee
ashkanbossani <abossani@hotmail.com>
2022-09-20 15:46:04 -0700

    fixed messages route naming error

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,2 +1,22 @@
-module.exports.addMessage = async (req, res, next) => {}
+const messageModel = require('../model/messageModel');
+
+
+module.exports.addMessage = async (req, res, next) => {
+    try {
+       const {from, to, message} = req.body;
+       const data = await messageModel.create({
+              message:{text:message},
+              users:[from, to],
+              sender:from,
+       })
+       if(data) return res.json({msg:"Message added successfully"});
+       return res.json({msg:"Message not added"});
+    } catch (ex) {
+        next(ex);
+    }
+}
+
+
+
+
 module.exports.getAllMessage = async (req, res, next) => {}
