0a812c3e8f48d5e08d58a6cb04c995fdd59dd977
ashkanbossani <abossani@hotmail.com>
2022-09-23 12:13:44 -0700

    added getAllMessages to message Controller to recieve messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,22 +1,36 @@
-const messageModel = require('../model/messageModel');
-
+const messageModel = require("../model/messageModel");
 
 module.exports.addMessage = async (req, res, next) => {
-    try {
-       const {from, to, message} = req.body;
-       const data = await messageModel.create({
-              message:{text:message},
-              users:[from, to],
-              sender:from,
-       })
-       if(data) return res.json({msg:"Message added successfully"});
-       return res.json({msg:"Message not added"});
-    } catch (ex) {
-        next(ex);
-    }
-}
+  try {
+    const { from, to, message } = req.body;
+    const data = await messageModel.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+    if (data) return res.json({ msg: "Message added successfully" });
+    return res.json({ msg: "Message not added" });
+  } catch (ex) {
+    next(ex);
+  }
+};
 
-
-
-
-module.exports.getAllMessage = async (req, res, next) => {}
+module.exports.getAllMessage = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+    const messages = await messageModel
+      .find({
+        users: { $all: [from, to] },
+      })
+      .sort({ updatedAt: 1 });
+    const projectMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
